5b72f92287fadef18bb97499c060a71d80a9f957
whanna <whanna@medicalsysconsult.com>
2016-03-19 16:22:51 -0400

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a href="{{ url('/') }}" class="pull-left"><img src="{{url('img/logo.png')}}"></a>
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" style="margin-left:3px;" href="{{ url('/') }}">
                     Alert Systems International
                 </a>
             </div>
