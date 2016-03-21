21e676fbe26ddb7bf268a7dc3e79e29f9b80a005
whanna <whanna@medicalsysconsult.com>
2016-03-21 03:30:56 -0400

    update

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 
     <h3 class="page-header resource-title">Standard Rig Safety Application</h3>
-    <object data="Brochure - Standard Rig Safety.pdf" type="application/pdf" width="100%" height="750px"> 
+    <object data="{{url('img/Brochure - Standard Rig Safety.pdf')}}" type="application/pdf" width="100%" height="750px"> 
 		<p>It appears you don't have a PDF plugin for this browser.
 		<a href="{{url('img/Brochure - Standard Rig Safety.pdf')}}">click here to
 		download the PDF file.</a></p>  
@@ -16,16 +16,16 @@
 	</div>
 
     <h3 class="page-header resource-title">Operator Alert Application</h3>
-    <object data="Brochure - Operator Alert System.pdf" type="application/pdf" width="100%" height="1500px"> 
+    <object data="{{url('img/Brochure - Operator Alert System.pdf')}}" type="application/pdf" width="100%" height="1500px"> 
 		<p>It appears you don't have a PDF plugin for this browser.
-		<a href="Brochure - Operator Alert System.pdf">click here to
+		<a href="{{url('img/Brochure - Operator Alert System.pdf')}}">click here to
 		download the PDF file.</a></p>  
 	</object>
             
     <h3 class="page-header resource-title">Recommended Safety System Bundles</h3>
-    <object data="PRODUCT PACKAGES.pdf" type="application/pdf" width="100%" height="850px"> 
+    <object data="{{url('img/PRODUCT PACKAGES.pdf')}}" type="application/pdf" width="100%" height="850px"> 
 		<p>It appears you don't have a PDF plugin for this browser.
-		<a href="PRODUCT PACKAGES.pdf">click here to
+		<a href="{{url('img/PRODUCT PACKAGES.pdf')}}">click here to
 		download the PDF file.</a></p>  
 	</object>
 </div>
