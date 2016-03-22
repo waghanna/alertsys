dd3ef80c842e507e4adb13285b8efb6e730af915
whanna <whanna@medicalsysconsult.com>
2016-03-22 18:45:10 -0400

    a

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss')
-    	.browserify('app.js');
-    	.version();
+    mix.sass('app.scss');
+	mix.browserify('app.js');
+	mix.version(["css/app.css", "js/app.js"]);
 });
