788fe38c73dc4c33766412a622391fbc353ee71c
whanna <whanna@medicalsysconsult.com>
2016-03-22 23:40:30 -0400

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>Alert Systems International</title>
 
     <!-- Fonts -->
@@ -13,7 +14,6 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-inverse navbar-static-top">
