7a63e5da910594296ea7b87ca0dbcec553c7b633
whanna <whanna@medicalsysconsult.com>
2016-03-23 00:56:33 -0400

    fix formatting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,15 @@
             <div id="home-img-text" class="home-img-overlay"></div>
             <div class="home-text product-group-block">
                 <div class="well">
-                    <h3>CANADIAN MADE OILFIELD DRILLING TECHNOLOGY</h3>
-                    <strong>When there's no margin for error and so much at stake, Alert Systems insures everything goes right.</strong><br>
-                    View our <a href="{{url('products')}}">PRODUCTS</a> and <a href="{{url('contact')}}">CONTACT</a> us to get started on your rig refurbishment or upgrade needs.<br><br>
-                    <p class="text-left">
+                    <div class="home-text-title">
+                        <h3>CANADIAN MADE OILFIELD DRILLING TECHNOLOGY</h3>
+                        <strong>When there's no margin for error and so much at stake, Alert Systems insures everything goes right.</strong><br>
+                        View our <a href="{{url('products')}}">PRODUCTS</a> and <a href="{{url('contact')}}">CONTACT</a> us to get started on your rig refurbishment or upgrade needs.
+                    </div>
+                    <br>
+                    <div>
                         Alert Systems International, Inc. is a leading global provider of advanced safety systems and telemetry solutions for drilling and service rigs. We offer a wide range of control products, electrical services, and custom mechanical fabrication services. Headquartered in Edmonton, Alberta with global sales, support and manufacturing operations in Boston and Dubai.
-                    </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -43,7 +46,6 @@
     <?php
         $dir = ('./img/display');
         $array_of_files_temp = preg_grep('/^([^.])/', scandir($dir));;
-        // shuffle($array_of_files_temp);            /* uncomment this line to randomize */
         $array_of_files= implode("\n", $array_of_files_temp);
         $array_of_files_temp = json_encode($array_of_files);
         echo ($array_of_files_temp);
